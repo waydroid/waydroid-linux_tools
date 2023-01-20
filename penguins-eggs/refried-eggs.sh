aeceb54908afa120d203bcc11ee0b8af199b48e4
Jon West <electrikjesus@gmail.com>
2023-01-20 16:46:33 -0500

    Workflow updates and branding
    This change includes a number of workflow updates as well as changes to include the wallpapers, nemo actions, etc. that we include in Waydroid-Linux builds.

--- penguins-eggs/refried-eggs.sh
+++ penguins-eggs/refried-eggs.sh
@@ -21,9 +21,16 @@ Whi='\e[0;37m';     BWhi='\e[1;37m';    UWhi='\e[4;37m';    IWhi='\e[0;97m';    
 # Define variables
 
 # home_folder="$(eval echo "~$USER")"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 username="$(who mom likes | sed -n 's/^\([^ ]*\).*/\1/p')"
-build_folder="/home/$username/refried-eggs/out"
-home_folder="/home/$username"
+if [ ! $username ]; then
+   username=$(echo "$USER")
+fi
+pre_home_folder="/home/$username"
+home_folder=$(echo $pre_home_folder | sed 's%/$%%')
+build_folder="$home_folder/refried-eggs/out"
+temp_folder="$home_folder/refried-eggs/tmp"
+base_build_folder="$home_folder/refried-eggs"
 BLOCKLIST="/etc/skel/.config/geany /etc/skel/.config/GitKraken \
 /etc/skel/.config/Google /etc/skel/.config/BraveSoftware \
 /etc/skel/.config/chromium /etc/skel/.config/gsconnect \
@@ -31,11 +38,21 @@ BLOCKLIST="/etc/skel/.config/geany /etc/skel/.config/GitKraken \
 /etc/skel/.config/libaccounts-glib \
 /etc/skel/.config/python_keyring /etc/skel/.config/fontconfig \
 /etc/skel/.local/share/kwalletd /etc/skel/.local/share/Google \
-/etc/skel/.local/share/tracker /etc/skel/.local/share/krita* "
+/etc/skel/.local/share/tracker /etc/skel/.local/share/krita* \
+/etc/skel/snap/gitkraken /etc/skel/snap/firefox \
+/etc/skel/var/lib/flatpak/app/com.visualstudio.code"
 external_packages="gitkraken rpi-imager imager stacer bleachbit code"
 omitted_packages="filezilla filezilla-common gnome-builder ubuntu-cleaner"
 wd_data_loc="/var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid"
-ignore_these_apps=""
+added_apps="nomachine"
+added_snap_apps="gitkraken firefox chromium"
+ommitted_snap_apps="gitkraken"
+added_flatpak_apps="com.visualstudio.code org.chromium.Chromium"
+
+echo "Script Directory: $SCRIPT_DIR"
+echo "Home Folder: $home_folder"
+echo "Build Folder: $build_folder"
+echo ""
 
 # Functions
 
@@ -49,16 +66,43 @@ doGenerateBackup(){
    mkdir -p $home_folder/backup/skel/.local/share
    mkdir -p $home_folder/backup/skel/.config
    mkdir -p $home_folder/backup/skel/.cache
-   sudo cp -Rp $home_folder/.themes $home_folder/backup/skel/
-   sudo cp -Rp $home_folder/.icons $home_folder/backup/skel/
+
+   # themes & icons if any
+   if [ -d $home_folder/.themes ]; then
+      sudo cp -Rp $home_folder/.themes $home_folder/backup/skel/
+   fi
+   if [ -d $home_folder/.icons ]; then
+      sudo cp -Rp $home_folder/.icons $home_folder/backup/skel/
+   fi
+   if [ -d $home_folder/.local/share/icons ]; then
+      sudo cp -Rp $home_folder/.local/share/icons $home_folder/backup/skel/.local/share/
+   fi
+
+   # backgrounds (part 1)
+   if [ ! -d $home_folder/.backgrounds ]; then
+      cp -Rp $SCRIPT_DIR/skel/.backgrounds $home_folder
+      sudo cp -Rp $SCRIPT_DIR/skel/.backgrounds/* /usr/share/backgrounds
+   fi
    sudo cp -Rp $home_folder/.backgrounds $home_folder/backup/skel/
+
+   # applications
+   if [ ! -d $home_folder/.local/share/applications ]; then
+      cp -Rp $SCRIPT_DIR/skel/.local/share/applications $home_folder.local/share/
+   fi
    sudo cp -Rp $home_folder/.local/share/applications $home_folder/backup/skel/.local/share/
+
+   # backgrounds (part 2)
+   if [ ! -d $home_folder/.local/share/backgrounds ]; then
+      cp -Rp $SCRIPT_DIR/skel/.local/share/backgrounds $home_folder.local/share/
+   fi
    sudo cp -Rp $home_folder/.local/share/backgrounds $home_folder/backup/skel/.local/share/
-   sudo cp -Rp $home_folder/.local/share/icons $home_folder/backup/skel/.local/share/
+
+   # any custom extensions or actions for nemo & nautilus as well
    sudo cp -Rp $home_folder/.local/share/nautilus $home_folder/backup/skel/.local/share/
    sudo cp -Rp $home_folder/.local/share/nautilus-python $home_folder/backup/skel/.local/share/
    sudo cp -Rp $home_folder/.local/share/nemo $home_folder/backup/skel/.local/share/
    sudo cp -Rp $home_folder/.local/share/nemo-python $home_folder/backup/skel/.local/share/
+
    # Now for the rest of the customizations
    sudo cp -Rp $home_folder/.local/bin $home_folder/backup/skel/.local/
    sudo cp -Rp $home_folder/.local/share/gnome* $home_folder/backup/skel/.local/share/
@@ -66,13 +110,17 @@ doGenerateBackup(){
    sudo cp -Rp $home_folder/.local/share/evolution $home_folder/backup/skel/.local/share/
    sudo cp -Rp $home_folder/.local/share/flatpak $home_folder/backup/skel/.local/share/
    sudo cp -Rp $home_folder/.local/share/webkitgtk $home_folder/backup/skel/.local/share/
-   sudo cp -Rp $home_folder/.local/share/waydroid $home_folder/backup/skel/.local/share/
+   if [ -d $home_folder/.local/share/waydroid ]; then
+      sudo cp -Rp $home_folder/.local/share/waydroid $home_folder/backup/skel/.local/share/
+   fi
+
    # Now for the configs
    sudo cp -Rp $home_folder/.config/gnome* $home_folder/backup/skel/.config/
    sudo cp -Rp $home_folder/.config/weston.ini $home_folder/backup/skel/.config/
    sudo cp -Rp $home_folder/.cache/gnome* $home_folder/backup/skel/.cache/
    sudo cp -Rp $home_folder/.cache/waydroid* $home_folder/backup/skel/.cache/
    sudo cp -Rp $home_folder/waydroid-package-manager $home_folder/backup/skel/
+   sudo chown -R $username $home_folder/backup
    echo -e "${IGre}  All set. You can find what was copied in $home_folder/backup ${RCol}"
 
 }
@@ -92,6 +140,8 @@ doGenerateSkelAndLocals(){
    sudo rm -rf $build_folder/*.iso
    sudo rm -rf $build_folder/*.sha
    sudo rm -rf /etc/skel/
+   sudo rm -rf /var/local/yolk
+   sudo rm -rf /usr/local/yolk
    echo -e "${IYel}  Rebuilding skel and locals ${RCol}"
    sudo eggs calamares --install
    sudo eggs tools:skel
@@ -99,24 +149,59 @@ doGenerateSkelAndLocals(){
    echo -e "${IYel}  Removing blocklisted items ${RCol}"
    sudo rm -rf $BLOCKLIST
    echo -e "${IYel}  Updating yolk ${RCol}"
-   if [[ ! -d /usr/local/yolk ]] && [[ -d /var/local/yolk ]]; then
-         sudo rm -rf /usr/local/yolk
-         sudo cp -r /var/local/yolk /usr/local/
+   sudo apt --purge autoremove
+   if [ ! -d /var/local/yolk ]; then
+      echo "Generating new yolk"
+      sudo eggs dad -d
+   elif [[ ! -d /usr/local/yolk ]] && [[ -d /var/local/yolk ]]; then
+      echo "Copying yolk now"
+      sudo cp -r /var/local/yolk /usr/local/
+   elif [[ -d /usr/local/yolk ]] && [[ -d /var/local/yolk ]]; then
+      echo "Refreshing yolk now"
+      sudo rm -rf /usr/local/yolk
+      sudo cp -r /var/local/yolk /usr/local/
    fi
-   read -p "press any key to continue"
+   # read -p "press any key to continue"
 }
 
 # Cleanup old eggs packaged iso, then regenerate skell and repackage the iso
 doWipeAndRebuild(){
    doGenerateSkelAndLocals
+
+   # Remove added apps
+   sudo apt remove $added_apps
+   # Remove added snap apps
+      sudo mv $home_folder/snap $temp_folder
+   # Move ommitted apps
+   for oapp in $ommitted_snap_apps; do
+      mkdir -p $temp_folder/osnap
+      sudo mv /var/lib/snap/$oapp $temp_folder/osnap/
+   done
+   # Remove added flatpak apps
+   for pack in $added_flatpak_apps; do
+      mkdir -p $temp_folder/flatpak
+      sudo mv /var/lib/flatpak/app/$pack $temp_folder/flatpak/
+   done
+   # remove any added bloat before packaging
+   sudo rm -rf /home/eggs/*.iso 
    echo -e "${IYel}  Refrying your eggs ${RCol}"
-   sudo eggs dad -d
    sudo eggs dad
    echo -e "${IYel}  Moving .iso to a folder you can access ${RCol}"
    sudo mv /home/eggs/*.iso $build_folder
    sudo chown $username $build_folder/*.iso
    echo -e $(ls -a $build_folder/*.iso)
    echo -e "${IYel}  Restoring ommitted packages ${RCol}"
+   sudo apt install $added_apps
+   # Remove added snap apps
+   sudo mv $temp_folder/snap $home_folder
+   # Move ommitted apps
+   for oapp in $ommitted_snap_apps; do
+      sudo mv $temp_folder/osnap/$oapp /var/lib/snap/
+   done   
+   # Remove added flatpak apps
+   for pack in $added_flatpak_apps; do
+      sudo mv $temp_folder/flatpak/$pack /var/lib/flatpak/app/
+   done
    # Install
    sudo apt update
    sudo apt --fix-broken install
@@ -130,13 +215,20 @@ if [ ! -f /usr/bin/eggs ]; then
 	return 1
 fi
 
-# Ask what command they want
-
+# Verify folders are created and properly owned
 if [ ! -d $build_folder ]; then
    echo "mkdir -p $build_folder"
+   mkdir -p $base_build_folder
    mkdir -p $build_folder
-   # mkdir -p $(eval echo "~$USER")/.cache/refried-eggs
+   sudo chown -R $username $base_build_folder
 fi 
+if [ ! -d $temp_folder ]; then
+   echo "mkdir -p $temp_folder"
+   mkdir -p $temp_folder
+   sudo chown -R $username $temp_folder
+fi 
+
+# Ask what command they want
 
 # Display Options
 
@@ -150,14 +242,19 @@ read -p "Input 1, 2, 3 or 4 for your selection: " choice
 
 case $choice in
   1) echo -e "${IYel} $choice ${RCol}"
-     doWipeAndRebuild ;;
+     doWipeAndRebuild 
+     echo "";;
   2) echo -e "${IYel} $choice ${RCol}"
-     doGenerateBackup ;;
+     doGenerateBackup 
+     echo "";;
   3) echo -e "${IYel} $choice ${RCol}"
-     doWipeWaydroidAndReinit ;;
+     doWipeWaydroidAndReinit 
+     echo "";;
   4) echo -e "${IYel} $choice ${RCol}"
-     doGenerateSkelAndLocals ;;
-  *) echo "Unrecognized selection: $choice" ;;
+     doGenerateSkelAndLocals 
+     echo "";;
+  *) echo "Unrecognized selection: $choice"
+     echo "" ;;
 esac
 
 
